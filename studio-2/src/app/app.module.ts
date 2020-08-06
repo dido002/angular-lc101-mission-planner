1d1963d56397fd329cb3205efae45d175ed06718
Umer <an01985i@gmail.com>
2020-08-06 18:10:18 -0500

    Modified

--- studio-2/src/app/app.module.ts
+++ studio-2/src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CountriesComponent } from './countries/countries.component';
+import { CrewComponent } from './crew/crew.component';
+import { EquipmentComponent } from './equipment/equipment.component';
+import { ExperimentsComponent } from './experiments/experiments.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CountriesComponent,
+    CrewComponent,
+    EquipmentComponent,
+    ExperimentsComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
